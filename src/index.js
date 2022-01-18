3248422ecc2a5089670b7ff792353d4069e42ab4
dajhelyan <assiaxramx10@gmail.com>
2022-01-17 21:17:54 -0500

    Adding header component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './components/Header'
 import reportWebVitals from './reportWebVitals';
-
+import  'bootstrap/dist/css/bootstrap.min.css' ;
 ReactDOM.render(
   <React.StrictMode>
+    <Header/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
