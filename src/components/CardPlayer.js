c4ba79d45a3db546dcbaf1f9736c70adda6f3c07
dajhelyan <assiaxramx10@gmail.com>
2022-01-18 20:29:47 -0500

    Adding components of top players

---
+++ src/components/CardPlayer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+
+const CardPlayer = ({data}) => {
+  return (
+    <Card style={{ width: "8rem" }} >
+      <Card.Img variant="top" src={data.avatar} rounded/>
+      <Card.Body>
+        <Card.Title className="p-text" >{data.nickname}</Card.Title>
+        <Card.Text>
+         <p className="p-text">{data.ranking}</p>
+         <p className="p-text">{data.status}</p>
+        </Card.Text>
+        <Button variant="primary">Go </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default CardPlayer;
