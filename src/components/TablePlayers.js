ad68bb5c79ce6d574836022ec190a5f0002487df
dajhelyan <assiaxramx10@gmail.com>
2022-01-22 19:09:04 -0500

    Adding table and pagination

--- src/components/TablePlayers.js
+++ src/components/TablePlayers.js
@@ -1,58 +1,33 @@
 import React from "react";
-import { useTable } from 'react-table'
+import { useTable, usePagination } from 'react-table'
 
-const TablePlayer = () => {
-  const data = React.useMemo(() => [
-      {
-        col1: 'Hello',
-        col2: 'World',
-      },
-      {
-        col1: 'react-table',
-        col2: 'rocks',
-      },
-      {
-        col1: 'whatever',
-        col2: 'you want',
-      },
-    ],
-    []
-  )
+const TablePlayer = ({columns, data}) => {
 
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: '#',
-        accessor: 'col1', // accessor is the "key" in the data
-      },
-      {
-        Header: 'Nickname',
-        accessor: 'col2',
-      },
-      {
-        Header: 'Status',
-        accessor: 'col1', // accessor is the "key" in the data
-      },
-      {
-        Header: 'Ranking',
-        accessor: 'col2',
-      },
-    ],
-    []
-  )
-  const tableInstance = useTable({ columns, data })
 
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
     prepareRow,
-  } = tableInstance
+    page, // Instead of using 'rows', we'll use page,
+    // which has only the rows for the active page
+
+    // The rest of these things are super handy, too ;)
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable({ columns, data }, usePagination)
   
 
   return (
 // apply the table props
+<>
    <table {...getTableProps()}>
      <thead>
        {// Loop over the header rows
@@ -72,28 +47,64 @@ const TablePlayer = () => {
      </thead>
      {/* Apply the table body props */}
      <tbody {...getTableBodyProps()}>
-       {// Loop over the table rows
-       rows.map(row => {
-         // Prepare the row for display
-         prepareRow(row)
-         return (
-           // Apply the row props
-           <tr {...row.getRowProps()}>
-             {// Loop over the rows cells
-             row.cells.map(cell => {
-               // Apply the cell props
-               return (
-                 <td {...cell.getCellProps()}>
-                   {// Render the cell contents
-                   cell.render('Cell')}
-                 </td>
-               )
-             })}
-           </tr>
-         )
-       })}
+       
+      {!data ? `<p>Sin resultados</p>` : (page.map((row, i) => {
+        prepareRow(row)
+        return (
+          <tr {...row.getRowProps()}>
+            {row.cells.map(cell => {
+              return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+            })}
+          </tr>
+        )
+      }))}
      </tbody>
    </table>
+    <div className="pagination">
+    <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+      {'<<'}
+    </button>{' '}
+    <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+      {'<'}
+    </button>{' '}
+    <button onClick={() => nextPage()} disabled={!canNextPage}>
+      {'>'}
+    </button>{' '}
+    <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+      {'>>'}
+    </button>{' '}
+    <span>
+      Page{' '}
+      <strong>
+        {pageIndex + 1} of {pageOptions.length}
+      </strong>{' '}
+    </span>
+    <span>
+      | Go to page:{' '}
+      <input
+        type="number"
+        defaultValue={pageIndex + 1}
+        onChange={e => {
+          const page = e.target.value ? Number(e.target.value) - 1 : 0
+          gotoPage(page)
+        }}
+        style={{ width: '100px' }}
+      />
+    </span>{' '}
+    {/* <select
+      value={pageSize}
+      onChange={e => {
+        setPageSize(Number(e.target.value))
+      }}
+    >
+      {[10, 20, 30, 40, 50].map(pageSize => (
+        <option key={pageSize} value={pageSize}>
+          Show {pageSize}
+        </option>
+      ))}
+    </select> */}
+  </div>
+   </>
   )
 }
 
